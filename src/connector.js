e5a457e570099323919d1587b9a54c5f9eb16cf6
Abhishek Singh <84718593+abhsin96@users.noreply.github.com>
2022-12-22 23:03:18 +0530

    initial commit

--- src/connector.js
+++ src/connector.js
@@ -1,17 +1,20 @@
+// const mongodb = require('mongodb');
+const dotenv = require("dotenv");
+// const mongoURI = "mongodb://localhost:27017" + "/collegeDetails";
+dotenv.config();
+const mongoURI = process.env.DATABASE_URL;
 
-//const mongodb = require('mongodb');
+let mongoose = require("mongoose");
+const { collegeSchema } = require("./schema");
 
-const mongoURI = "mongodb://localhost:27017" + "/collegeDetails"
-
-let mongoose = require('mongoose');
-const { collegeSchema } = require('./schema')
-
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
-    });
-let collegeModel = mongoose.model('collegerecords', collegeSchema)
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connection established with mongodb server online");
+  })
+  .catch((err) => {
+    console.log("error while connection", err);
+  });
+let collegeModel = mongoose.model("collegerecords", collegeSchema);
 
 exports.connection = collegeModel;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,63 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const dotenv = require("dotenv");
+// const { connections } = require("mongoose");
+const { connection } = require("./connector");
+
+dotenv.config();
+// const port = process.env.PORT || 8080;
+const port = 8080;
+
+// following parameters: name, state, city, minPackage, maxFees, course and exams
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connections } = require('mongoose');
 
+app.get("/findColleges", async (req, res) => {
+  const {
+    name = "",
+    state = "",
+    city = "",
+    minPackage = 0,
+    maxFees = 0,
+    course = "",
+    exams = [],
+  } = req.body;
+  //   console.log(name, state, city, minPackage, maxFees, course, exams);
+  console.log(req.body);
+  try {
+    if (+maxFees > 0 && +minPackage > 0) {
+      const query = {};
 
+      if (name) {
+        query["name"] = { $regex: name };
+      }
+      if (state) {
+        query["state"] = { $regex: state };
+      }
+      if (city) {
+        query["city"] = { $regex: city };
+      }
+      if (course) {
+        query["course"] = { $regex: course };
+      }
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+      query["minPackage"] = { $gte: +minPackage };
+      query["maxFees"] = { $gte: +maxFees };
+
+      const result = await connection.find(query);
+      res.status(200).json({ result });
+    } else {
+      throw new Error("intentional error");
+      res.status(400).json({ error: "Invalid Payload" });
+    }
+  } catch (error) {
+    res.status(500).json({ status: false, error: "internal server error" });
+  }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
